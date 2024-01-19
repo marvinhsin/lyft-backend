bb73c9f393b7e3b038703129a17006ebd7a24bda
Marvin Hsin <bdfb1997@berkeley.edu>
2024-01-19 01:29:41 -0600

    refactor codes

---
+++ car_factory.py
@@ -0,0 +1,34 @@
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+
+def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    engine = CapuletEngine(current_mileage, last_service_mileage)
+    battery = SpindlerBattery(current_date, last_service_date)
+    return Car(engine, battery)
+
+def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    engine = WilloughbyEngine(current_mileage, last_service_mileage)
+    battery = SpindlerBattery(current_date, last_service_date)
+    return Car(engine, battery)
+
+def create_palindrome(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    engine = SternmanEngine(warning_light_is_on)
+    battery = SpindlerBattery(current_date, last_service_date)
+    return Car(engine, battery)
+
+def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    engine = WilloughbyEngine(current_mileage, last_service_mileage)
+    battery = NubbinBattery(current_date, last_service_date)
+    return Car(engine, battery)
+
+def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    engine = CapuletEngine(current_mileage, last_service_mileage)
+    battery = NubbinBattery(current_date, last_service_date)
+    return Car(engine, battery)
+
+
